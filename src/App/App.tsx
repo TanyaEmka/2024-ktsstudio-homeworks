3843cf187d6ff077288e468e0e950dc7f793a156
unknown <tanyaemelyanova2015@gmail.com>
2024-04-21 15:42:23 +0300

    change project structure

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from '../assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import 'styles/styles.scss';
+
+import Text from 'components/Text';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +31,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Text view='title'>Заголовок</Text>
     </>
   )
 }
 
-export default App
+export default App;
