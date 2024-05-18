c6b787147b525bc607d4fa466297e6573bee6d77
unknown <tanyaemelyanova2015@gmail.com>
2024-05-18 17:37:55 +0300

    add meal plan types

--- src/types/apiTypes.ts
+++ src/types/apiTypes.ts
@@ -70,6 +70,106 @@ export type MenuItemUnit = {
     servings: ServingsType,
 }
 
+export type MealPlanNutrinitionUnitType = {
+    name: string,
+    amount: number,
+    unit: string,
+    percentOfDailyNeeds: number    
+}
+
+export type NutritionSummaryType = {
+    nutrients: MealPlanNutrinitionUnitType[]
+}
+
+export type MealPlanItemKind = 
+    'INGREDIENTS' | 
+    'PRODUCT' | 
+    'MENU_ITEM' | 
+    'RECIPE' | 
+    'CUSTOM_FOOD';
+
+export interface MealPlanItemType {
+    id: number,
+    slot: number,
+    position: 1,
+    type: MealPlanItemKind,
+}
+
+export interface MealPlanRecipeType extends MealPlanItemType {
+    type: 'RECIPE',
+    value: {
+        id: number,
+        servings: number,
+        title: string,
+        imageType: string,
+        image: string,
+    }
+}
+
+export interface MealPlanProductType extends MealPlanItemType {
+    type: 'PRODUCT',
+    value: {
+        id: number,
+        servings: number,
+        title: string,
+        imageType: string,
+        image: string,
+    }
+}
+export interface MealPlanMenuItemType extends MealPlanItemType {
+    type: 'MENU_ITEM',
+    value: {
+        id: number,
+        servings: number,
+        title: string,
+        imageType: string,
+        image: string,
+    }
+}
+export interface MealPlanCustomFoodType extends MealPlanItemType {
+    type: 'CUSTOM_FOOD',
+    value: {
+        id: number,
+        servings: number,
+        title: string,
+        imageType: string,
+        image: string,
+    }
+}
+export interface MealPlanIngredientsType extends MealPlanItemType {
+    type: 'INGREDIENTS',
+    value: {
+        id: number,
+        servings: number,
+        title: string,
+        imageType: string,
+        image: string,
+    }
+}
+
+export type MealPlanDayType = {
+    nutritionSummary: NutritionSummaryType,
+    nutritionSummaryBreakfast: NutritionSummaryType,
+    nutritionSummaryLunch: NutritionSummaryType,
+    nutritionSummaryDinner: NutritionSummaryType,
+    date: number,
+    day: string,
+    items: MealPlanItemType[],
+}
+
+export type MealPlanWeekType = {
+    days: MealPlanDayType[],
+}
+
+export type MealPlanTemplateType = {
+    id: number,
+    name: string,
+}
+
+export type MealPlanTemplatesType = {
+    templates: MealPlanTemplateType[],
+}
+
 export type Status = {
     statusName: 'LOADING' | 'ERROR' | 'SUCCESS' | 'NOT_STARTED',
     statusMessage: string,
