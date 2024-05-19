a6b6abd2750a30da6c94fd1de24b3ecb83068b99
unknown <tanyaemelyanova2015@gmail.com>
2024-05-19 22:26:46 +0300

    add styles for user page

--- src/App/pages/User/User.tsx
+++ src/App/pages/User/User.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import userStore from "store/UserStore";
 import { useNavigate } from "react-router-dom";
 import { observer } from "mobx-react-lite";
+import styles from './User.module.scss';
 
 import Text from "components/Text";
 import Button from "components/Button";
@@ -19,17 +20,27 @@ const User: React.FC = () => {
     }, [userStatus]);
 
     return (
-        <div className="user-page">
+        <div className={styles["user-page"]}>
             <Text view='title'>User Page</Text>
-            {Object.entries(user).map(([key, value]) => {
-                return (
-                    <div className="user-page-unit" key={key}>
-                        <Text weight="bold" view='p-16'>{key}:</Text>
-                        <Text view='p-16'>{value}</Text>
-                    </div>
-                )
-            })}
-            <Button onClick={() => { logout() }}>
+            <div
+                className={styles["user-page__info"]} 
+            >
+                {Object.entries(user).map(([key, value]) => {
+                    return (
+                        <div 
+                            className={styles["user-page__info__unit"]} 
+                            key={key}
+                        >
+                            <Text weight="bold" view='p-16'>{key}:</Text>
+                            <Text view='p-16'>{value}</Text>
+                        </div>
+                    )
+                })}
+            </div>
+            <Button
+                className={styles["user-page__logout"]} 
+                onClick={() => { logout() }}
+            >
                 Logout
             </Button>
         </div>
